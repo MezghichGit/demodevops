31a7a5206af3a557c9914c27946a10a885c9692c
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-05-26 09:13:01 +0100

    version 1 : page index

---
+++ src/main/java/com/gk/git/DemoBitbucketApplication.java
@@ -0,0 +1,13 @@
+package com.gk.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBitbucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBitbucketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gk/git/DemoBitbucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBitbucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
